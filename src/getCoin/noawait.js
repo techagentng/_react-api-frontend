8bb2dcbe7ca6bdc24bd2aaace8b03574161041bd
Capabay <capabay@hotmail.com>
2021-03-13 18:48:13 +0100

    await again

--- src/getCoin/noawait.js
+++ src/getCoin/noawait.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 export default class Noawait extends React.Component{   
     //Declare state
-   
+   state = {
+       loading: false,
+   }
     async componentDidMount(){
         const url = "https://cors-anywhere.herokuapp.com/localbitcoins.com/buy-bitcoins-online/NGN/.json";
         const response = await fetch(url);
         const data = await response.json();
+        console.log(data);
         this.setState({profile: data.data.ad_list[5].data.temp_price}); 
     }
   
     render(){
-        return <div> Loading...</div>;        
+        return <div>{this.state.loading ? <div>heyyyyy true</div> : <div>Loading...</div>}</div>;        
     }
 }
 
